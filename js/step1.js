5b35067720ed2c359ec8ad7a0c23075d192493d3
HJK-Z <hjk.businessemail@gmail.com>
2021-01-09 18:59:43 -0800

    fixes

--- js/step1.js
+++ js/step1.js
@@ -2,10 +2,29 @@
 var form = document.getElementById("form");
 form.addEventListener('submit', function (event) { event.preventDefault(); });
 
+// disabling form submissions if there are invalid fields
+function checkValid() {
+    var elements = document.getElementById("form").elements;
+
+    var valid = true;
+    for (var i = 0, element; element = elements[i++];) {
+        if (element.classList.contains("form-control") && element.value === "") {
+            valid = false;
+            break;
+        }
+    }
+
+    return valid;
+}
+
 // save info to local storage
 var submitbtn = document.getElementById("submit");
 
 submitbtn.addEventListener("click", function () {
+    if (!checkValid()) {
+        return;
+    }
+
     var name = document.getElementById("name");
     var pronoun = document.getElementById("pronoun");
     var likes = document.getElementById("likes");
